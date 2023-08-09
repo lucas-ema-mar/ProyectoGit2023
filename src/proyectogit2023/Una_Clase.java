57e6ffa6f06c22d815ca4cc31d0f3da83afd0e3c
YoNahum <nahumrecalde@yahoo.com.ar>
2023-08-09 11:32:37 -0300

    Clase

---
+++ src/proyectogit2023/Una_Clase.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyectogit2023;
+
+/**
+ *
+ * @author Hogar
+ */
+public class Una_Clase {
+
+}
